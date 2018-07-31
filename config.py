480fc86af05edc1e003956cf65805e402340cfd3
Tiffany Chien <tiffanychien@Tiffanys-MacBook-Pro.local>
2018-07-31 07:47:05 -0700

    complete

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "ENTER YOUR KEY HERE!"
+consumer_secret = "ENTER YOUR KEY HERE!"
+access_token = "ENTER YOUR KEY HERE!"
+access_token_secret = "ENTER YOUR KEY HERE!"
